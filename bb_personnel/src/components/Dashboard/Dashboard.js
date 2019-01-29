9f8f9c946f6175528c17d3c910297e84ae3a2841
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-29 15:40:22 -0500

    Added dashboard and display components to start

---
+++ bb_personnel/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Dashboard;
