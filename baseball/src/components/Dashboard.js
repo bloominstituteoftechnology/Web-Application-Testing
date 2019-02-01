8c0c3383e9a3fcfb7c003b88ff14d93ed2c74966
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-31 22:53:31 -0600

    Components built out and MVP styling finished

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+
+import Display from "./Display";
+
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-top: 25px;
+`;
 
 export default class Dashboard extends Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+    hit: false,
+    foul: false
+  };
+
   render() {
     return (
       <div>
-        <h1>Dashboard</h1>
+        <Display />
+        <ButtonContainer>
+          <Button variant="danger" size="lg" style={{ marginBottom: "10px" }}>
+            Strike!
+          </Button>
+          <Button
+            variant="secondary"
+            size="lg"
+            style={{ marginBottom: "10px" }}
+          >
+            Ball!
+          </Button>
+          <Button variant="warning" size="lg" style={{ marginBottom: "10px" }}>
+            Foul!
+          </Button>
+          <Button variant="success" size="lg" style={{ marginBottom: "10px" }}>
+            Hit!
+          </Button>
+        </ButtonContainer>
       </div>
-    )
+    );
   }
 }
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import { Image } from "react-bootstrap";
+import scoreboard from "../assets/scoreboard.jpg";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+`;
 
 export default class Display extends Component {
   render() {
     return (
-      <div>
-        <h1>Display</h1>
-      </div>
+      <Container>
+        <Image src={scoreboard} alt="baseball scoreboard" fluid />
+      </Container>
     );
   }
 }
