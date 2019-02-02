e5b6023c429b0fbf9326ca3defed7429134ff85e
Julie Jonak <juliejonak10@gmail.com>
2019-02-02 13:30:36 -0600

    Setup counters: - App.js: buttons increase counters and reset to zero when appropriate - App.test.js: wrote initial tests for ball incrementing

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -10,14 +9,44 @@ class App extends Component {
     hits: 0,
   }
 
-  raise = (e) => {
-    
-    let newValue = ++e.target.value
-    this.setState(
-      ({
-        [e.target.name] : newValue
+  ball = (e) => {
+    let demBalls = this.state.balls;
+    if(demBalls < 4){
+      this.setState({
+        balls: demBalls+1
       })
-    )
+    } else {
+      this.setState({
+        balls: 0
+      })
+    }
+  }
+
+  strike = (e) => {
+    let demStrikes = this.state.strikes;
+    if(demStrikes < 3){
+      this.setState({
+        strikes: demStrikes+1
+      })
+    } else {
+      this.setState({
+        strikes: 0
+      })
+    }
+  }
+
+  foul = (e) => {
+    let demFouls = this.state.fouls;
+      this.setState({
+        fouls: demFouls+1
+      })
+  }
+
+  hit = (e) => {
+      this.setState({
+        balls: 0,
+        strikes: 0
+      })
   }
 
   render() {
@@ -30,10 +59,17 @@ class App extends Component {
 
         <h3>Strikes: {this.state.strikes}</h3>
 
+        <h3>Fouls: {this.state.fouls}</h3>
+
+        <h3>Hits: {this.state.hits}</h3>
+
         <h5>You control the game</h5>
 
         <div>
-          <button name="balls" onClick={this.raise}>Hit</button>
+          <button className="ball" onClick={this.ball}>Ball</button>
+          <button className="strike" onClick={this.strike}>Strike</button>
+          <button className="foul" onClick={this.foul}>Foul</button>
+          <button className="hit" onClick={this.hit}>Hit</button>
         </div>
 
       </div>
--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -6,3 +6,37 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App />)
 });
+
+it('starts with state set to zero', () => {
+  const wrapper = shallow(<App />);
+  const instance = wrapper.instance();
+
+  expect(instance.state.balls).toBe(0);
+  expect(instance.state.strikes).toBe(0);
+  expect(instance.state.fouls).toBe(0);
+  expect(instance.state.hits).toBe(0);
+})
+
+describe('the ball button', () => {
+  it('should increment by 1 with each click and reset to 0 after 4', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    const button = wrapper.find('button.ball')
+
+    button.simulate('click');
+    expect(instance.state.balls).toBe(1);
+
+    button.simulate('click');
+    expect(instance.state.balls).toBe(2);
+
+    button.simulate('click');
+    expect(instance.state.balls).toBe(3);
+
+    button.simulate('click');
+    expect(instance.state.balls).toBe(4);
+
+    button.simulate('click');
+    expect(instance.state.balls).toBe(0);
+  })
+})
