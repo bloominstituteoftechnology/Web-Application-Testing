32d383ce4aa1d0cd8ed80cf6fbfe844d5277d92f
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-02 12:49:22 -0500

    strikes and ball functions complete

--- baseball/src/components/dashboard.js
+++ baseball/src/components/dashboard.js
@@ -1,71 +1,82 @@
 import React, { Component } from 'react'
 import Display from './display.js'
 
- class Dashboard extends Component {
-constructor(props){
-super(props);
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
 
-this.state = {
-    balls: 0,
-    strikes: 0,
-    outs: 0,
-}
-this.strike = this.strike.bind(this);
-this.isInningOver = this.isInningOver.bind(this);
+    this.state = {
+      balls: 0,
+      strikes: 0,
+      outs: 0,
+    }
+    this.isInningOver = this.isInningOver.bind(this);
+   
 
-}
+  }
 
-strike = () =>{
-if (this.state.strikes <2){
- this.setState(prevState =>({
-     strikes: prevState.strikes + 1
- }))
-} else if (this.state.strikes === 2){
-    this.setState(prevState => ({
+  strike = () => {
+    if (this.state.strikes < 2) {
+      this.setState(prevState => ({
+        strikes: prevState.strikes + 1
+      }))
+    } else {
+      alert('strikeout');
+      this.setState(prevState => ({
         balls: 0,
-        strikes:0,
-        outs: prevState.outs + 1
-    }));
-}
- this.isInningOver();
-}
+        strikes: 0,
+        outs: ++prevState.outs
+      }));
+    }
+   setTimeout(() => {
+   this.isInningOver();
+   }, 1000);
+  }
 
+  isInningOver() {
+    if (this.state.outs === 3) {
+      alert('Inning Over');
+      this.setState(prevState => ({
+        balls: 0,
+        strikes: 0,
+        outs: 0
+      }))
+    }
+  }
 
-isInningOver(){
-    if (this.state.outs === 3){
-        alert('Inning Over');
-        this.setState(prevState => ({
-            balls:0,
-            strikes:0,
-            outs:0
-        }))
-}
-}
-
-ball(){
-
+  ball() {
+  if(this.state.balls < 3){
+    this.setState(prevState => ({
+      balls: prevState.balls + 1 
+    }))
+  } else  if (this.state.balls === 3){alert('Walk'); this.setState({
+    balls: 0,
+    strikes: 0
+  })
+  }
 }
 
 foul = () => {
 
-}
+  }
 
-hit = () => {
+  hit = () => {
 
-}
+  }
 
 
   render() {
-    
+    { console.log(this.state) }
     return (
       <div>
-          <Display count={this.state}/>
-          <button onClick={() => this.strike()}>Strike</button>
-          <button>Ball</button>
-          <button>Foul</button>
-          <button>Hit</button>
-        
+        <Display count={this.state} />
+        <button onClick={() => this.strike()}>Strike</button>
+        <button onClick={() => this.ball()}>Ball</button>
+        <button>Foul</button>
+        <button>Hit</button>
+
       </div>
+     
     )
   }
 }
