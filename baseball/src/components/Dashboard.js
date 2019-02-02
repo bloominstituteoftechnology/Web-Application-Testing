7e95a66c39eaf9d350d779eefa28d78dfc0acc6e
alasalle <angela.lasalle91@gmail.com>
2019-02-02 13:53:40 -0500

    completed Dashboard component

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -4,7 +4,12 @@ import React from "react";
 class Dashboard extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <button data-testid="Strike" onClick={this.props.callStrike}>Strike</button>
+        <button data-testid="Ball" onClick={this.props.callBall}>Ball</button>
+        <button data-testid="Foul" onClick={this.props.callFoul}>Foul</button>
+        <button data-testid="Hit" onClick={this.props.hit}>Hit</button>
+      </div>
     )
   }
 }
