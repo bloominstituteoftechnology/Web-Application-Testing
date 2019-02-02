2bac3b97be09a61f5a690c04cfc00869d09342bb
iepoch <staclem@live.com>
2019-02-02 12:52:52 -0700

    Fixed to see the Balls and Strikes reset. And what it currently set too

--- baseball/src/Components/Display/Display.test.js
+++ baseball/src/Components/Display/Display.test.js
@@ -5,43 +5,45 @@ import "jest-dom/extend-expect";
 import Dashboard from "../Dashboard/Dashboard";
 
 describe("Display Test", () => {
-it("renders without crashing", () => {
+  it("renders without crashing", () => {
     const div = document.createElement("div");
     render(<Display />, div);
-});
+  });
 
-it("displays the ball count", () => {
+  it("displays the ball count", () => {
     const component = render(<Display />);
-    const score = component.getByTestId(/balls/i);
-    expect(score).toHaveTextContent("0");
-});
+      const balls = component.getByTestId(/balls/i);
+      const strikes = component.getByTestId(/strikes/i);
+      expect(strikes).toHaveTextContent("0");
+      expect(balls).toHaveTextContent("0");
+  });
 
-it("should increase ball count onClick", () => {
+  it("should increase ball count onClick", () => {
     const component = render(<Dashboard />);
     const ballBtn = component.getByTestId("ball-btn");
     fireEvent.click(ballBtn);
     const display = render(<Display />);
     const score = display.getByTestId(/balls/i);
     expect(score).toHaveTextContent("1");
-});
-it("should increase strike count onClick", () => {
+  });
+  it("should increase strike count onClick", () => {
     const component = render(<Dashboard />);
     const strikeBtn = component.getByTestId("strike-btn");
     fireEvent.click(strikeBtn);
     const display = render(<Display />);
     const score = display.getByTestId(/strikes/i);
     expect(score).toHaveTextContent("1");
-});
-it("should increase strike < 2 when foulBtn is clicked", () => {
+  });
+  it("should increase strike < 2 when foulBtn is clicked", () => {
     const component = render(<Dashboard />);
     const foulBtn = component.getByTestId("foul-btn");
     fireEvent.click(foulBtn);
     const display = render(<Display />);
     const score = display.getByTestId(/strikes/i);
     expect(score).toHaveTextContent("2");
-});
+  });
 
-it("should reset strikes and balls when hitBtn is clicked", () => {
+  it("should reset strikes and balls when hitBtn is clicked", () => {
     const component = render(<Dashboard />);
     const hitBtn = component.getByTestId("hit-btn");
     fireEvent.click(hitBtn);
@@ -50,9 +52,9 @@ it("should reset strikes and balls when hitBtn is clicked", () => {
     const strikes = display.getByTestId(/strikes/i);
     expect(balls).toHaveTextContent("0");
     expect(strikes).toHaveTextContent("0");
-});
+  });
 
-it("should reset strikes and balls when >= 3", () => {
+  it("should reset strikes and balls when >= 3", () => {
     const component = render(<Dashboard />);
     const strikeBtn = component.getByTestId("strike-btn");
     fireEvent.click(strikeBtn);
@@ -62,9 +64,9 @@ it("should reset strikes and balls when >= 3", () => {
     const display = render(<Display />);
     const score = display.getByTestId(/strikes/i);
     expect(score).toHaveTextContent("0");
-});
+  });
 
-it("should reset balls and strikes whe >=4 && strikes >= 3", () => {
+  it("should reset balls and strikes whe >=4 && strikes >= 3", () => {
     const component = render(<Dashboard />);
     const ballBtn = component.getByTestId("ball-btn");
     const strikeBtn = component.getByTestId("strike-btn");
@@ -76,7 +78,9 @@ it("should reset balls and strikes whe >=4 && strikes >= 3", () => {
     fireEvent.click(strikeBtn);
     fireEvent.click(ballBtn);
     const display = render(<Display />);
-    const score = display.getByTestId(/strikes/i);
-    expect(score).toHaveTextContent("0");
-});
+    const strikes = display.getByTestId(/strikes/i);
+    const balls = display.getByTestId(/balls/i);
+    expect(strikes).toHaveTextContent("0");
+    expect(balls).toHaveTextContent("0");
+  });
 });
