b91e00806fd66a3690cc15da36407b59ec55e83c
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-02 10:11:19 -0800

    Basic initialization started. Initial commit.

---
+++ ui/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h1>Dashboard</h1>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ ui/src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = (props) => {
+    return (
+        <h5>Display</h5>
+    )
+}
+
+export default Display;
