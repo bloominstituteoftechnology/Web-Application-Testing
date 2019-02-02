5842586608561af3fcc9d0e947a484f28881e8ae
Michael Trew <mtrew2015@gmail.com>
2019-02-02 04:26:13 -0500

    deleted some files

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import 'jest-dom/extend-expect';
 import {render} from 'react-testing-library';
 it('renders without crashing', () => {
   render(<App/>)
