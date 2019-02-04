c94b9fafcb9a6a754757f673c95b014381c5f257
Freekeyboy <jjhutton81@gmail.com>
2019-02-04 12:42:53 -0800

    Created react app. Removed unneeded code. Created needed components.

---
+++ bace-ball-tracker/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Dashboard from "./components/Dashboard";
+import Display from "./components/Display";
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+        <Display />
+      </div>
+    );
+  }
+}
+
+ export default App;
---
+++ bace-ball-tracker/src/components/Display.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Display extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Display</h1>
+      </div>
+    );
+  }
+}
