5e15102a66b60e1f5c427ed10d1308beace37534
Freekeyboy <jjhutton81@gmail.com>
2019-02-04 13:57:55 -0800

    Added Dashboard testing.

--- bace-ball-tracker/src/App.js
+++ bace-ball-tracker/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Dashboard from "./components/Dashboard";
+import Display from "./components/Display";
 
  class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
         <Display />
       </div>
     );
--- bace-ball-tracker/src/components/Display.js
+++ bace-ball-tracker/src/components/Display.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Image } from "react-bootstrap";
 import scoreboard from "../assets/scoreboard.jpg";
 
+import Dashboard from "./Dashboard";
+import { strike, ball, foul, hit } from "../helpers/playBall";
+
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -9,12 +12,70 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
+const NumberContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Number = styled.div`
+  font-size: 48px;
+  padding: 20px;
+  text-align: center;
+`;
+
 export default class Display extends Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+    hits: 0,
+    fouls: 0,
+    outs: 0
+  };
+
+  callStrike = event => {
+    event.preventDefault();
+    this.setState(strike(this.state));
+    console.log(this.state);
+  };
+
+  callBall = event => {
+    event.preventDefault();
+    this.setState(ball(this.state));
+    console.log(this.state);
+  };
+
+  callFoul = event => {
+    event.preventDefault();
+    this.setState(foul(this.state));
+    console.log(this.state);
+  };
+
+  callHit = event => {
+    event.preventDefault();
+    this.setState(hit(this.state));
+    console.log(this.state);
+  };
   render() {
     return (
-      <Container>
-        <Image src={scoreboard} alt="baseball scoreboard" fluid />
-      </Container>
+      <div>
+        <Container>
+          <Image src={scoreboard} alt="baseball scoreboard" fluid />
+        </Container>
+        <NumberContainer>
+          <Number>Balls: {this.state.balls}</Number>
+          <Number>Strikes: {this.state.strikes}</Number>
+          <Number>Hits: {this.state.hits}</Number>
+          <Number>Fouls: {this.state.fouls}</Number>
+          <Number>Outs: {this.state.outs}</Number>
+        </NumberContainer>
+        <Dashboard
+          callStrike={this.callStrike}
+          callBall={this.callBall}
+          callFoul={this.callFoul}
+          callHit={this.callHit}
+        />
+      </div>
     );
   }
 }
