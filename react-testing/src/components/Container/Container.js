7b51023b1293eea396886671fb993226eb9c7fdb
vy3191 <venkyafhs@gmail.com>
2019-02-06 11:13:11 -0500

    Added reset functionality

--- react-testing/src/components/Container/Container.js
+++ react-testing/src/components/Container/Container.js
@@ -10,30 +10,71 @@ export default class Container extends Component {
      hits:0
   }
 
-  reset = () => {
-     if(this.state.balls >=4 || this.state.strikes >= 3) {
-         this.setState({
-           balls:0,
-           strikes:0
-        });
-     }
-  }
-  hitHandler = () => {
-    if(this.state.hits>0) {
-      this.setState({
-         balls:0,
-         strikes:0
-      });
+  resetBalls = event => {
+    event.preventDefault();
+    if (this.state.balls >= 4 && this.state.strikes >= 3) {
+        this.setState({
+            balls: 0,
+            strikes: 0
+        })
     }
-  }
+    else{
+        this.setState({
+            ...this.state,
+            balls:this.state.balls + 1
+        })
+    }
+
+};
+
+  resetStrikes = (event) => {
+    event.preventDefault();
+    if (this.state.balls >= 4 && this.state.strikes >= 3) {
+        this.setState({
+            ...this.state,
+            balls: 0,
+            strikes: 0
+        })
+    }
+    else {
+        this.setState({
+            balls: this.state.balls,
+            strikes: this.state.strikes + 1
+        })
+    }
+};
+
+resetHits = event=> {
+    event.preventDefault()
+    this.setState({
+        balls: 0,
+        strikes:0
+    })
+};
+
+  resetFouls = event => {
+    event.preventDefault();
+    if (this.state.strikes < 2) {
+        this.setState({
+            ...this.state,
+            strikes: this.state.strikes + 1
+        })
+    }
+};
+
   render() {
     return (
       <div>
-        <Dashboard reset={this.reset}/>
-        <Display 
-           balls={this.state.balls}
-           strikes={this.state.strikes}
-        />
+          <Dashboard resetBalls={this.resetBalls}
+                    resetStrikes ={this.resetStrikes}
+                    resetHits={this.resetHits}
+                    resetFouls={this.resetFouls}
+                    />
+          <Display   resetBalls={this.resetBalls}
+                    resetStrikes ={this.resetStrikes}
+                    resetHits={this.resetHits}
+                    resetFouls={this.resetFouls}
+                      />
       </div>
     )
   }
