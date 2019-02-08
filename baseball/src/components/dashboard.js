0d853b37f369a5dad8e573f6bc28bc2ebfc9da5e
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-07 22:03:09 -0500

    fixed formatting

--- baseball/src/components/dashboard.js
+++ baseball/src/components/dashboard.js
@@ -4,17 +4,13 @@ import React, { Component } from 'react'
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       balls: 0,
       strikes: 0,
       outs: 0,
     }
     this.isInningOver = this.isInningOver.bind(this);
-   
-
   }
-
   strike = () => {
     if (this.state.strikes < 2) {
       this.setState(prevState => ({
@@ -27,14 +23,13 @@ class Dashboard extends Component {
         outs: ++prevState.outs
       }));
     }
-   setTimeout(() => {
-   this.isInningOver();
-   }, 1000);
+    setTimeout(() => {
+      this.isInningOver();
+    }, 1000);
   }
 
   isInningOver() {
     if (this.state.outs === 3) {
-      
       this.setState(prevState => ({
         balls: 0,
         strikes: 0,
@@ -44,48 +39,41 @@ class Dashboard extends Component {
   }
 
   ball() {
-  if(this.state.balls < 3){
-    this.setState(prevState => ({
-      balls: prevState.balls + 1 
-    }))
-  } else  if (this.state.balls === 3){this.setState({
-    balls: 0,
-    strikes: 0
-  })
+    if (this.state.balls < 3) {
+      this.setState(prevState => ({
+        balls: prevState.balls + 1
+      }))
+    } else if (this.state.balls === 3) {
+      this.setState({
+        balls: 0,
+        strikes: 0
+      })
+    }
   }
-}
-
-foul = () => {
- if(this.state.strikes < 2){
-     this.setState(prevState => ({
-         strikes: prevState.strikes + 1
-     }))
- }
-
+  foul = () => {
+    if (this.state.strikes < 2) {
+      this.setState(prevState => ({
+        strikes: prevState.strikes + 1
+      }))
+    }
   }
-
   hit = () => {
     this.setState({
-        balls: 0,
-        strikes: 0,
+      balls: 0,
+      strikes: 0,
     })
   }
-
-
   render() {
-   
     return (
       <div>
         <button onClick={() => this.strike()}>Strike</button>
         <button onClick={() => this.ball()}>Ball</button>
         <button onClick={() => this.foul()}>Foul</button>
-        <button onClick= {() => this.hit()}>Hit</button>
+        <button onClick={() => this.hit()}>Hit</button>
         <h2 data-testid='strikes'>{this.state.strikes}</h2>
         <h2 data-testid='balls'>{this.state.balls}</h2>
         <h2 data-testid='outs'>{this.state.outs}</h2>
-
       </div>
-     
     )
   }
 }
