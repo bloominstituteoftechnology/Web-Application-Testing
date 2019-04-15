f5fa8949e87187c087d7cf2ecd3d8aa2f7c19c78
Jesse Theisen <theiseja@gmail.com>
2019-04-14 21:55:23 -0500

    MVP: Added tests, simple styling will be added also

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -2,8 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'jest-dom/extend-expect';
+import {render, fireEvent} from 'react-testing-library';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
+
+
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
 });
+
+describe('The App Component', () => {
+  const app = render(<App />);
+  const displayComponent = render(<Display />);
+  const dashboardComponent = render(<Dashboard />);
+
+  it('it exists', () => {
+    expect(app).toBeTruthy();
+  })
+  it('contains content', () => {
+    expect.anything();
+  })
+  it('contains needed sub-components', () => {
+    expect(displayComponent).toBeTruthy();
+    expect(dashboardComponent).toBeTruthy();
+  })
+  const ballsDiv = app.getByText(/balls/i);
+  const strikesDiv = app.getByText(/strikes/i);
+
+  describe('ball & strike keywords', () => {
+    it('element contains balls keyword specifically', () => {
+      expect(ballsDiv).toHaveTextContent(/balls/i);
+    })
+    it('contains strikes specifically', () => {
+      expect(strikesDiv).toHaveTextContent(/strikes/i);
+    })
+  })
+
+  describe('if the buttons work correctly', () => {
+    test('ball button works successfully', () => {
+      const ballButton = dashboardComponent.getByTitle('ballButton');
+        fireEvent.click(ballButton);
+        const ballCount = displayComponent.getByTitle('balls-count');
+        expect(ballCount).toHaveTextContent('1');
+    })
+    test('if the strike button works', () => {
+      const strikeButton = dashboardComponent.getByTitle('strikeButton');
+      fireEvent.click(strikeButton);
+      const strikeCount = displayComponent.getByTitle('strikes-count');
+      expect(strikeCount).toHaveTextContent('1')
+    })
+
+    test('that strikes successfully reset when they reach 3', () => {
+      const strikeButton = dashboardComponent.getByTitle('strikeButton');
+        fireEvent.click(strikeButton);
+        fireEvent.click(strikeButton); //fireEvent keeps log of past uses (line 62) so only 2 was needed in this test to simulate 3 button clicks.
+        const strikeCount = displayComponent.getByTitle('strikes-count');
+        expect(strikeCount).toHaveTextContent('0');
+    })
+  })
+})
