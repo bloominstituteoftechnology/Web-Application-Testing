e82870916a0aa21100b715e13c1e154735269b4d
Jesse Theisen <theiseja@gmail.com>
2019-04-14 21:36:24 -0500

    MVP: Completed Display and Dashboard components, implementing tests now

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -20,5 +20,38 @@ class App extends Component {
       }
     })
   };
+
+  strikeIncrease = e => {
+    this.setState(prevState => {
+      return {
+        strikes: prevState.strikes + 1,
+      }
+    })
+  };
+
+  hitButton = e => {
+    this.setState({
+      balls: 0,
+      strikes: 0,
+    })
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if((this.state.balls > 3) || (this.state.strikes > 2)) {
+      this.setState({
+        balls: 0,
+        strikes: 0,
+      })
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+      <Display ballCount = {this.state.balls} strikeCount = {this.state.strikes} />
+      <Dashboard ballAction = {this.ballIncrease} strikeAction = {this.strikeIncrease} foulAction = {this.ballIncrease} hitAction = {this.hitButton} />
+      </div>
+    )
+  }
 }
 export default App;
