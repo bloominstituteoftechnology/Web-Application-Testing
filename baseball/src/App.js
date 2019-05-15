d745dc00bd3b16d054666cac095d2697d372b09f
jkaddu <jkaddu@yahoo.com>
2019-05-15 15:16:10 -0700

    balls and strikes added, working on test

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<h1> Baseball</h1>
-			<p>Strikes</p>
-			<button>Strike</button>
-			<p>Balls</p>
-			<button>Ball</button>
-		</div>
-	);
+class App extends Component {
+	state = {
+		strike: 0,
+		ball: 0
+	};
+
+	addStrike = () => {
+		this.setState({
+			strike: this.state.strike + 1
+		});
+	};
+
+	addBall = () => {
+		this.setState({
+			ball: this.state.ball + 1
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1> Baseball</h1>
+				<p>Strikes</p>
+				<p>Count: {this.state.count}</p>
+				<button onClick={this.addStrike}>Strike</button>
+				<p>Balls</p>
+				<p>Count: {this.state.balls}</p>
+				<button>Ball</button>
+			</div>
+		);
+	}
 }
 
 export default App;
--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+	it('renders without crashing', () => {
+		render(<App />);
+	});
+
+	it('should display strikes and balls', () => {
+		const { getByText } = render(<App />);
+
+		getByText(/strikes/i);
+
+		getByText(/balls/i);
+	});
+
+	it('should add strikes and balls', () => {
+		const { getByText } = render(<App />);
+
+		const button = getByText(/strike/i);
+		fireEvent.click(button);
+	});
 });
