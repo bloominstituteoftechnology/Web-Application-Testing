7cb8b9eac36fb282b9c22e8ac2059ef49eec3eeb
jkaddu <jkaddu@yahoo.com>
2019-05-15 12:58:57 -0700

    app created

---
+++ baseball/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1> Baseball</h1>
+			<p>Strikes</p>
+			<button>Strike</button>
+			<p>Balls</p>
+			<button>Ball</button>
+		</div>
+	);
+}
+
+export default App;
