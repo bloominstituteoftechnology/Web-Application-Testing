bb3c23e43dd391514a903f0d06bbfe68fbefa4be
dangolbeeker <scottb305@outlook.com>
2019-08-08 18:25:31 -0400

    scott-beeker

--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Display (){
+export default function Display (props){
     return(
         <div>
             <h1>Display</h1>
+            <div>
+                {props.balls}
+                {props.strikes}
+            </div>
+
         </div>
     )
 }
