f7cebd3edf3636c23680c9d99e0c146b632310f4
dangolbeeker <scottb305@outlook.com>
2019-08-08 18:38:17 -0400

    striken ball, and foul logic added

--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -7,6 +7,8 @@ export default function Display (props){
             <div>
                 {props.balls}
                 {props.strikes}
+                {props.fouls}
+                {props.hits}
             </div>
 
         </div>
