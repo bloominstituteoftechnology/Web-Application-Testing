0f01ae517ccb5ab4ebb28d25b7a7a1f868313906
dangolbeeker <scottb305@outlook.com>
2019-08-08 18:17:22 -0400

    setting up Dashboard

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Display from './components/Display';
+//import Display from './components/Display';
 import Dashboard from './components/Dashboard';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>TESTING APP</h1>
-      <Display />
+      <h1>TESTING</h1>
+
+      {/* <Display /> */}
       <Dashboard />
     </div>
   );
 }
-
 export default App;
--- web-application-testing/src/components/Dashboard.js
+++ web-application-testing/src/components/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Display from './Display';
 
 export default function Dashboard () {
+
+    const [balls, setBalls] = useState(0);
+    const [strikes, setStrikes] = useState(0);
+
     return(
         <div>
             <h1>Dashboard</h1>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>hit</button>
+            <Display balls={balls}
+                     setBalls={setBalls}
+                     strikes={strikes}
+                     setStrikes={setStrikes} />
         </div>
     )
 }
