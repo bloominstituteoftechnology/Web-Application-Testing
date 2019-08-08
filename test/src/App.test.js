fecf672eaf75f97ed3c6ede5ccc4d7c2772e9491
Mimi Hoang <miminhoang1@gmail.com>
2019-08-08 18:16:54 -0400

    Testing

--- test/src/App.test.js
+++ test/src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, {render} from 'react';
 
+import App from './App';
+describe('<App />', () => {
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App/>);
 });
+it('should display strikes and balls', () => {
+  const { getByText } = render(<App />);
+
+   getByText(/strikes/i);
+
+   getByText(/balls/i);
+});
+
+ it('should add strikes and balls', () => {
+  const { getAllByText } = render(<App />);
+
+   const button = getAllByText(/strike/i);
+  fireEvent.click(button);
+
+  const button1 = getAllByText(/ball/i);
+  fireEvent.click(button1);
+});
+});	
+
