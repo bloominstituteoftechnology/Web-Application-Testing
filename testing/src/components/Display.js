494573e0bdac97bc4c2ff40d3c67140da885aff9
doanmade <43007082+doanmade@users.noreply.github.com>
2019-09-05 15:38:10 -0700

    Added home and away score and styling

--- testing/src/components/Display.js
+++ testing/src/components/Display.js
@@ -4,18 +4,24 @@ const Display = player => {
   return (
     <div>
       <h1>ScoreBoard</h1>
-
-      <h3>
-        Home Score: {player.homeScore} {"  /  "} Away Score: {player.awayScore}
-      </h3>
-
-      <h3>Total Hits: {player.hits}</h3>
-
-      <h3>Ball: {player.ball}</h3>
-
-      <h3>Strike: {player.strike}</h3>
-
-      <hr />
+      <div className="scoreBoard">
+        <div className="score">
+          <h2 className="home">
+            Home Score: <br />
+            <span className="points">{player.homeScore}</span>
+          </h2>
+          <h2 className="away">
+            Away Score: <br />
+            <span className="points">{player.awayScore}</span>
+          </h2>
+        </div>
+        <hr />
+        <h3>Total Hits: {player.hits}</h3>
+        <hr />
+        <h3>Ball: {player.ball}</h3>
+        <hr />
+        <h3>Strike: {player.strike}</h3>
+      </div>
     </div>
   );
 };
