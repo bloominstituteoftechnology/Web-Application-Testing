941387984d829559bb403b9db8fd261c1be31703
seongck <seongk@pm.me>
2019-09-12 18:52:01 -0700

    Set up Dashboard and Display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
 
 function App() {
   return (
     <div className="App">
+      <Display />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Display() {
+  return (
+    <div>
+      <h1>Baseball Dashboard</h1>
+    </div>
+  )
+}
+
+export default Display;
