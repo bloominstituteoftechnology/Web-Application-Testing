f3fd293beba11678673108db016e8fa8196be78c
seongck <seongk@pm.me>
2019-09-12 20:51:00 -0700

    Add tests for ball and strike resets

--- src/App.test.js
+++ src/App.test.js
@@ -8,14 +8,16 @@ test('renders without crashing', () => {
   render(<App />);
 });
 
-test('addBall adds 1 ball', () => {
+test('addBall adds 1 ball, and resets when it reaches 4 balls', () => {
   expect(addBall(0)).toBe(1);
   expect(addBall(2)).toBe(3);
+  expect(addBall(4)).toBe(0);
 });
 
-test('addStrike adds 1 strike', () => {
+test('addStrike adds 1 strike, and resets when it reaches 3 strikes', () => {
   expect(addStrike(2)).toBe(3);
   expect(addStrike(0)).toBe(1);
+  expect(addStrike(3)).toBe(0);
 });
 
 test('addFoul adds 1 foul', () => {
