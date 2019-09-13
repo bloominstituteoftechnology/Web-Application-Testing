fda779cdd3c20b3961d56484ec9f6765911825e8
seongck <seongk@pm.me>
2019-09-12 19:08:40 -0700

    Add score functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Display from './components/Display';
 
+export const addBall = balls => balls + 1;
+export const addStrike = strikes => strikes + 1;
+export const addFoul = fouls => fouls + 1;
+export const addHit = hits => hits + 1;
+
 function App() {
   const [ balls, setBalls ] = useState(0);
   const [ strikes, setStrikes ] = useState(0);
