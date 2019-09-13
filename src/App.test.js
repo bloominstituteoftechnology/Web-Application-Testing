fec3efbe87f002f2746b76d5e588c279c9728a2e
seongck <seongk@pm.me>
2019-09-12 20:07:05 -0700

    Add tests for all score functions

--- src/App.test.js
+++ src/App.test.js
@@ -8,5 +8,27 @@ test('renders without crashing', () => {
   render(<App />);
 });
 
+test('addBall adds 1 ball', () => {
+  expect(addBall(0)).toBe(1);
+  expect(addBall(2)).toBe(3);
+});
+
+test('addStrike adds 1 strike', () => {
+  expect(addStrike(2)).toBe(3);
+  expect(addStrike(0)).toBe(1);
+});
+
+test('addFoul adds 1 foul', () => {
+  expect(addFoul(0)).toBe(1);
+  expect(addFoul(2)).toBe(3);
+});
+
+test('addHit adds 1 hit', () => {
+  expect(addHit(0)).toBe(1);
+  expect(addHit(1)).toBe(2);
+});
+
+
+
 
 
