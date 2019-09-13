7b43381c81633586050b789c24438678dcc96179
seongck <seongk@pm.me>
2019-09-12 19:06:25 -0700

    Add state and props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import Dashboard from './components/Dashboard';
 import Display from './components/Display';
 
 function App() {
+  const [ balls, setBalls ] = useState(0);
+  const [ strikes, setStrikes ] = useState(0);
+  const [ fouls, setFouls ] = useState(0);
+  const [ hits, setHits ] = useState(0);
+
   return (
     <div className="App">
-      <Display />
-      <Dashboard />
+      <Display props={{
+        balls,
+        setBalls,
+        strikes,
+        setStrikes,
+        fouls,
+        setFouls,
+        hits,
+        setHits
+      }} />
+      <Dashboard props={{
+        balls,
+        setBalls,
+        strikes,
+        setStrikes,
+        fouls,
+        setFouls,
+        hits,
+        setHits
+      }} />
     </div>
   );
 }
