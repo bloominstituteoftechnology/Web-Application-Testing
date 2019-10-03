e5c3c258e29ad02e9cc485bfd1c48cc225c57692
Adam Payne <ajpayne238@gmail.com>
2019-10-03 19:17:05 -0400

    passed test should fail

--- baseball/src/Display.js
+++ baseball/src/Display.js
@@ -1,7 +1,7 @@
 // - display the count of `balls` and `strikes` for the at-bat.
 // - should be updated when the user records activity on the `Dashboard` component.
 import { Dashboard } from "./Dashboard";
-import { addBalls, addStrikes} from "./Dashboard"
+
 
 import React from "react";
 
@@ -10,11 +10,7 @@ export function Display(){
     return (
         <div className="display">
             <h1>At Bat</h1>
-            <Dashboard 
-            
-            />
-            <p>{addBalls}</p>
-            <p>{addStrikes}</p>
+            <Dashboard />
 
         </div>
     )
