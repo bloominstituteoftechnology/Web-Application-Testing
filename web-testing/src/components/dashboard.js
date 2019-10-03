4ab3783ef103517ab7f806861930b6b58c042247
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-03 14:06:00 -0600

    added components, and built out basic functional components

---
+++ web-testing/src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dashboard(){
+    return(
+        <div>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ web-testing/src/components/display.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Display(){
+    return(
+        <div>
+            <h1>Current Player At Bat</h1>
+            <h3>Ball</h3>
+            <h4>Render the current count here</h4>
+            <h3>Strikes</h3>
+            <h4>Render the current count here</h4>
+        </div>
+    );
+}
+
+export default Display;
