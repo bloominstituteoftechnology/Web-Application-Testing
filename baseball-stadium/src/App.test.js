c14b156272e12f68c599883cc14c3b42d04e77eb
Oksana <oxanakril@gmail.com>
2019-10-10 21:25:25 -0500

    basic testing

--- baseball-stadium/src/App.test.js
+++ baseball-stadium/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
+
+afterEach(rtl.cleanup);
+
+test("Render the app", () =>{
+  const wrapper = rtl.render(<App />)
+  wrapper.debug()
+})
+
+test('contains foul and hit button', () => {
+	const containerDash = rtl.render(<Dashboard />);
+	containerDash.getByText(/hit/i);
+	containerDash.getByText(/foul/i);
 });
+
+// test('balls and strikes are equal', () => {
+//   const containerDash = rtl.render(<App />);
+//   const balls = containerDash.getByDisplayValue(/balls/i)
+// 	expect(balls).toBeLessThan(4)
+// });
