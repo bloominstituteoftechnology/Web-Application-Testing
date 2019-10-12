52bfb6d491d035f27a04163b3981c6782f30c872
spettigrew <sara.pettigrew5@gmail.com>
2019-10-11 19:31:26 -0600

    notes added

--- baseball-scoreboard/src/components/Dashboard.js
+++ baseball-scoreboard/src/components/Dashboard.js
@@ -1,26 +1,28 @@
 import React from 'react';
-// import Button from './Button';
 
-export function Dashboard() {
+function Dashboard({ buttonFunctions }) {
+    const labels = ['Strike', 'Ball', 'Foul', 'Reset'];
     return (
-        <div className='buttons'>
-            <ChangeButton
-            name='Strike'
-            value={props.strikes}
-            strike={props.strikes}
-            />
-        </div>
-    )
+        Object.values(buttonFunctions)
+            .map((button, i) => <button key={i} onClick={button}>{labels[i]}</button>)
+    );
 }
 
-    // import React from 'react';
 
-    // function Dashboard({ buttonFunctions }) {
-    //     const labels = ['Strike', 'Ball', 'Foul', 'Reset'];
-    //     return (
-    //         Object.values(buttonFunctions)
-    //             .map((button, i) => <button key={i} onClick={button}>{labels[i]}</button>)
-    //     );
-    // }
+// import React from 'react';
+// // import Button from './Button';
+
+// export function Dashboard() {
+//     return (
+//         <div className='buttons'>
+//             <ChangeButton
+//             name='Strike'
+//             value={props.strikes}
+//             strike={props.strikes}
+//             />
+//         </div>
+//     )
+// }
+
 
 export default Dashboard
