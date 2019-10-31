f5b76c83aa9a3751cb02a44d360d6cf115002c75
Rasha Moumneh <rasha@fastmail.com>
2019-10-31 17:51:37 -0400

    Added unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent, getByTestId } from '@testing-library/react'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('displays four buttons', () => {
+  const container = render(<App />)
+  container.getAllByRole('button')
+});
+
+test('has header for Balls', () => {
+  const container = render(<App />)
+  container.getByText('Balls')
+})
+
+test('has header for Strikes', () => {
+  const container = render(<App />)
+  container.getByText('Strikes')
+})
+
+
+test('button events fire', () => {
+  const container = render(<App />)
+  fireEvent.click(container.getByText('Strike'))
+  fireEvent.click(container.getByText('Hit'))
+  fireEvent.click(container.getByText('Foul'))
+  fireEvent.click(container.getByText('Balls'))
+})
+
+test("properly increments the strike and ball counter", () => {
+  const { getByText, getByTestId } = render(<App />);
+  const strikeCount = getByTestId("strikeCount");
+  const ballCount = getByTestId("ballCount");
+  const strikeButton = getByText("Strike");
+  const ballButton = getByText("Ball");
+  fireEvent.click(strikeButton);
+  fireEvent.click(ballButton);
+  expect(strikeCount.textContent).toEqual("1");
+  expect(ballCount.textContent).toEqual("1");
+});
+
+test("hit button properly resets all counters to 0", () => {
+  const { getByText, getByTestId } = render(<App />);
+  const strikeCount = getByTestId("strikeCount");
+  const ballCount = getByTestId("ballCount");
+  const hitButton = getByText("Hit");
+  fireEvent.click(hitButton);
+  expect(strikeCount.textContent).toEqual("0");
+  expect(ballCount.textContent).toEqual("0");
+});
+
+
+
+
+
+
+
+
+
--- src/components/Display.js
+++ src/components/Display.js
@@ -8,11 +8,11 @@ class Display extends React.Component {
     return (
       <div>
         <h2>Balls</h2>
-        <div>
+        <div data-testid='ballCount'>
           {this.props.count.balls}
         </div>
         <h2>Strikes</h2>
-        <div>
+        <div data-testid='strikeCount'>
           {this.props.count.strikes}
         </div>
       </div>
