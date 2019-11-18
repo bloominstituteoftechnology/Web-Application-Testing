9886b93f8d9e92239905fe17bd596683db02f975
shgradyy <shantaygrady@gmail.com>
2019-11-17 22:58:39 -0500

    App set-up, adds dependecies

---
+++ batter-up/src/App.js
@@ -0,0 +1,14 @@
+import Reactm {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
