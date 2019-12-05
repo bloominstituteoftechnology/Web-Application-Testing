1edf5b8e711a3cb49bceb4039a6f5355ab4b1fc1
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-05 17:20:38 -0500

    added in 2 tests in app.test.js

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Dashboard, {strikeCount} from './Components/Dashboard';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('App renders without crashing', () => {
+  render(<App />)
+})
+
+test('ball title rendered', () => {
+  const {getByText} = render(<App />);
+
+  getByText(/^ball$/i)
+})
