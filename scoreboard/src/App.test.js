d37fad610a10018013e3904b9d216b5783e052cc
jarrodskahill <jarrodskahill@gmail.com>
2019-12-05 15:26:27 -0800

    added a test checking if values are displayed

--- scoreboard/src/App.test.js
+++ scoreboard/src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   render(<App />);
 
 });
+
+test('renders scores', ()=> {
+  const {getByTestId} = render(<App />);
+  getByTestId(/ballValue/i);
+  getByTestId(/strikeValue/i);
+  
+})
--- scoreboard/src/Components/Display.js
+++ scoreboard/src/Components/Display.js
@@ -3,8 +3,8 @@ import React from "react"
 const ScoreDisplay = (props) => {
     return (
         <div>
-            <h1>Balls : {props.balls}</h1>
-            <h1>Strikes : {props.strikes}</h1>
+            <h1 data-testid='ballValues'>Balls : {props.balls}</h1>
+            <h1 data-testid='strikeValues'>Strikes : {props.strikes}</h1>
         </div>
     )
 }
