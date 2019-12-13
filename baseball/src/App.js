fb06e3c8abb3990afe92bfd12c9929c5235f6f97
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2019-12-12 19:12:06 -0500

    functionality completed

---
+++ baseball/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Baseball Tracker</h2>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
