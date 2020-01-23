e2ca4ca5f0371866602e9bb9d59f0414b53b61d3
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-22 22:44:29 -0800

    Added the Display and Dashboard content

--- test-web-application/src/App.js
+++ test-web-application/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+import { Display } from './components/Display.js';
+import { Dashboard } from './components/Dashboard.js'
+
+
 function App() {
+  const [ball, setBall] = useState(0);
+  const [strike, setStrke] =  useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         web-test application
-        </p>
-     
-      </header>
+      <Display 
+      atBallBall={ball}
+      atBallStrike ={strike}
+      />
+      <Dashboard
+      atBallBall={ball}
+      atBallStrike ={strike}
+      setBall={setBall}
+      setStrke={setStrke}
+      />
     </div>
   );
 }
