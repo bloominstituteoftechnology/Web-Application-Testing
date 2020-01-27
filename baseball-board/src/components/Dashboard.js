4a25a515cee9832a778411aef488efe34e8a7321
RoskiDeluge <roberto.delgado@gmail.com>
2020-01-26 16:07:40 -1000

    First commit, created Dashboard & Display comps

---
+++ baseball-board/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = () => {
+    
+    return (
+        <div>Dashboard here</div>
+    )
+};
+
+export default Dashboard;
---
+++ baseball-board/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Display = () => {
+    
+    return (
+        <div>Display here</div>
+    )
+};
+
+export default Display;
