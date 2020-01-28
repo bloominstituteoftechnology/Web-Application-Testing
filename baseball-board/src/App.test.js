7808eecd298c6c8bfdddffcfb334ab7d6bb536de
RoskiDeluge <roberto.delgado@gmail.com>
2020-01-27 14:30:37 -1000

    Added 1 unit test and 1 integration test

--- baseball-board/src/App.test.js
+++ baseball-board/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { addPitch } from './components/Dashboard/Dashboard';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pageTitle = getByText(/at bat dashboard/i);
+  expect(pageTitle).toBeInTheDocument();
+});
+
+test("pitch add 1", () => {
+  // ARRANGE
+  const currentCount = 1;
+  const expected = 2;
+
+  // ACT
+  const actual = addPitch(currentCount);
+
+  // ASSERT
+  expect(actual).toBe(expected);
 });
